cfa3cf9655a2b4e37185ba3a2f0b30866224e96b
BekirKagan <kagankaraahmetoglu@hotmail.com>
2024-10-20 00:57:51 +0300

    Implemented basic socket functionality and did minor UI changes.

--- bozcord-client/app/page.tsx
+++ bozcord-client/app/page.tsx
@@ -1,18 +1,37 @@
 "use client"
 
-import ChannelView from "@/components/ChannelView"
 import RoomsPanel from "@/components/RoomsPanel"
+import UserPanel from "@/components/UserPanel"
+import ChatView from "@/components/ChannelView"
 import UsersPanel from "@/components/UsersPanel"
 
+import { io, Socket } from "socket.io-client"
+import { useState, useEffect } from "react"
+
 export default function App() {
+  const [clientSocket, setClientSocket] = useState<Socket>()
+
+  useEffect(() => {
+    return () => {
+      // Socket Initialization
+      const socket = io("http://localhost:8000")
+      setClientSocket(socket)
+    }
+  }, [])
+
+  useEffect(() => {
+    return () => {
+      // Socket Events
+    }
+  }, [clientSocket])
+
   return (
     <div className="w-screen h-screen flex space-x-[10px] bg-dark-gray text-dark-primary-text p-[10px]">
       <div className="w-[350px] h-full flex flex-col space-y-[10px]">
         <RoomsPanel />
-        <div className="w-[350px] h-[60px] bg-dark-dark-gray rounded-b-xl">
-        </div>
+        <UserPanel />
       </div>
-      <ChannelView />
+      <ChatView />
       <UsersPanel />
     </div>
   )
